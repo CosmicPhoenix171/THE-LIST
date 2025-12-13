34793231ebd0a4d768f27482615d43d1c748c301
cosmicphoenix171 <fireball0000@hotmail.com>
2025-12-12 23:21:22 -0600

    feat: update ad image formats from SVG to PNG

--- app.js
+++ app.js
@@ -10012,13 +10012,13 @@ async function autoAddTmdbKeywordEntries(franchiseLabel, keywordInfo, entries, o
 // ============================================================================
 
 const adImages = [
-  'ads/ad1.svg',
-  'ads/ad2.svg',
-  'ads/ad3.svg',
-  'ads/ad4.svg',
-  'ads/ad5.svg',
-  'ads/ad6.svg',
-  'ads/ad7.svg',
+  'ads/ad1.png',
+  'ads/ad2.png',
+  'ads/ad3.png',
+  'ads/ad4.png',
+  'ads/ad5.png',
+  'ads/ad6.png',
+  'ads/ad7.png',
   // Add more ad images here as you add them to the ads folder
 ];
 
